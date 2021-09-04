be785d4985a3a39e1b22266dcd1730b07b8441b9
betsywenwen@yahoo.co.jp <betsywenwen@yahoo.co.jp>
2021-09-04 15:19:38 +0800

    8-4,controller层,api

---
+++ src/main/java/com/xuwen/javamall/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.xuwen.javamall.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * author:xuwen
+ * Created on 2021/9/4
+ */
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+
+}
--- src/test/java/com/xuwen/javamall/service/Impl/UserServiceImplTest.java
+++ src/test/java/com/xuwen/javamall/service/Impl/UserServiceImplTest.java
@@ -6,9 +6,11 @@ import com.xuwen.javamall.pojo.User;
 import com.xuwen.javamall.service.IUserService;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.Assert.*;
-
+//test是否回滚数据，@Transactional
+//@Transactional
 //测试方法类，需要继承父类测试方法，否则头上➕注解
 public class UserServiceImplTest extends JavamallApplicationTests {
     @Autowired
@@ -16,7 +18,7 @@ public class UserServiceImplTest extends JavamallApplicationTests {
 
     @Test
     public void register() {
-        User user = new User("zhangsan","123456","zhangsan@qq.com", RoleEnum.CUSTOMER.getCode());
+        User user = new User("lisi","123456","lisi@qq.com", RoleEnum.CUSTOMER.getCode());
         userService.register(user);
         System.out.println("数据注册成功！");
     }
